1ae38c5f9f7a80eeecebc4c8f25a0bd8f0cd851f
Kongpop(Kai) <ru.kongpop@hotmail.com>
2024-12-16 12:40:57 +0700

    [ADD] Search product filter
    -Search by product name
    -Search by product tag
    -Search by product category

--- pages/products.vue
+++ pages/products.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="p-6 bg-white rounded-lg">
     <h1 class="text-2xl font-bold">{{ $t('navbar.manageProducts') }}</h1>
-    <!-- Button for opening the add product form -->
-    <button @click="toggleForm" class="bg-[#1660f8] text-white my-7.5 px-4 py-2 rounded-lg flex items-center space-x-2">
-      <plusIcon class="w-5 h-5 text-current" />
-      {{ $t('button.productForm.addProduct') }}
-    </button>
+
+    <input
+      v-model="searchText"
+      type="text"
+      placeholder="Search product..."
+      class="input-style w-full flex-grow mt-5"
+    />
+    <div class="flex items-center justify-between space-x-4 my-5">
+      <div class="flex items-center space-x-4 flex-grow">
+        <multiselect
+          v-model="selectedCategory"
+          :options="categories"
+          :searchable="true"
+          placeholder="Filter by category"
+          :label="$i18n.locale"
+          :track-by="'id'"
+          :selectLabel="''"
+          class="w-1/4 flex-grow"
+        ></multiselect>
+        <multiselect
+          v-model="selectedTag"
+          :options="tags"
+          :searchable="true"
+          placeholder="Filter by tag"
+          :label="$i18n.locale"
+          :track-by="'id'"
+          :selectLabel="''"
+          class="w-1/4 flex-grow"
+        ></multiselect>
+      </div>
+      <button
+        @click="toggleForm"
+        class="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-800 flex items-center space-x-2 transition duration-300"
+      >
+        <plusIcon class="w-5 h-5 text-white" />
+        <span>{{ $t('button.productForm.addProduct') }}</span>
+      </button>
+    </div>
 
     <!-- Show product list -->
     <div class="overflow-x-auto">
-      <ProductList :products="products" @edit="handleEditProduct" @delete="handleDeleteProduct" />
+      <ProductList :products="filteredProducts" @edit="handleEditProduct" @delete="handleDeleteProduct" />
     </div>
 
     <!-- Form for adding/editing products -->
@@ -49,21 +82,43 @@
 <script>
 import ProductList from '~/components/ProductList.vue';
 import ProductForm from '~/components/ProductForm.vue';
+import Multiselect from 'vue-multiselect';
 import plusIcon from '~/assets/icon/plus.svg';
 
 export default {
   components: {
     ProductList,
     ProductForm,
-    plusIcon
+    plusIcon,
+    Multiselect,
   },
   data() {
     return {
       products: [],
+      categories: [],
+      tags: [],
+      searchText: '',
+      selectedCategory: null,
+      selectedTag: null,
       showForm: false,
       showCancelConfirmation: false,
       productToEdit: null,
-    }
+    };
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter((product) => {
+        const matchText = product.name.toLowerCase().includes(this.searchText.toLowerCase());
+        const matchCategory = this.selectedCategory
+          ? product.category.id === this.selectedCategory.id
+          : true;
+        const matchTag = this.selectedTag
+          ? product.tags.some((tag) => tag.id === this.selectedTag.id)
+          : true;
+
+        return matchText && matchCategory && matchTag;
+      });
+    },
   },
   methods: {
     toggleForm() {
@@ -71,40 +126,51 @@ export default {
       this.showCancelConfirmation = false;
     },
     confirmCancel() {
-      this.showForm = false
-      this.showCancelConfirmation = false
-      if (!this.showForm) this.productToEdit = null;
+      this.showForm = false;
+      this.showCancelConfirmation = false;
+      this.productToEdit = null;
     },
     handleAddProduct(newProduct) {
       this.products.push(newProduct);
       this.showForm = false;
     },
     handleUpdateProduct(updatedProduct) {
-      const index = this.products.findIndex(p => p._id === updatedProduct._id);
-      if (index !== -1) {
-        this.products.splice(index, 1, updatedProduct);
-      }
+      const index = this.products.findIndex((p) => p._id === updatedProduct._id);
+      if (index !== -1) this.products.splice(index, 1, updatedProduct);
       this.showForm = false;
       this.productToEdit = null;
     },
     handleDeleteProduct(productId) {
-      this.products = this.products.filter(product => product._id !== productId);
+      this.products = this.products.filter((product) => product._id !== productId);
     },
     handleEditProduct(product) {
       this.productToEdit = { ...product };
       this.showForm = true;
     },
+    async fetchData() {
+      try {
+        const [productRes, categoryRes, tagRes] = await Promise.all([
+          this.$axios.$get('/products'),
+          this.$axios.$get('/categories'),
+          this.$axios.$get('/tags'),
+        ]);
+
+        this.products = productRes;
+        this.categories = categoryRes.map((cat) => ({ ...cat, id: cat._id, ...cat.name }));
+        this.tags = tagRes.map((tag) => ({ ...tag, id: tag._id, ...tag.name }));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
   },
-  async fetch() {
-    try {
-      const response = await this.$axios.$get('/products')
-      this.products = response
-    } catch (error) {
-      console.error('Error fetching products:', error)
-    }
+  mounted() {
+    this.fetchData();
   },
-}
+};
 </script>
 
 <style scoped>
+.input-style {
+  @apply border border-gray-300 rounded-md px-3 py-2 focus:outline-none;
+}
 </style>
